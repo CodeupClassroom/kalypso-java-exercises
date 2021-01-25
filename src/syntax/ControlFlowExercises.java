2f55ad1700364471c53f2137502e9485580538e6
Daniel Fryar <daniel.fryar@gmail.com>
2021-01-25 10:19:25 -0600

    Finished first exercise

---
+++ src/syntax/ControlFlowExercises.java
@@ -0,0 +1,13 @@
+package syntax;
+
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
