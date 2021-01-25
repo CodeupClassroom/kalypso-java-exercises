846fe4a90354de0ce0d6fcd781a57b9ec31c40fa
Fernando Mendoza <fernandoza@gmail.com>
2021-01-25 10:04:02 -0600

    feat: add examples of string equalsIgnoreCase

--- src/StringsLec.java
+++ src/StringsLec.java
@@ -12,10 +12,16 @@ public class StringsLec {
         System.out.println("What's your name?");
         firstName = sc.nextLine();
 
+        //!firstName.equals("Fer") not equals
         if(firstName.equals("Fer")){
             System.out.println("firstName is Fer");
         } else {
             System.out.println("firstName is not Fer");
+        }
+
+        System.out.println("What's your email?");
+        if(sc.nextLine().equalsIgnoreCase("fer@codeup.com")){
+            System.out.println("You're an admin");
         }
 
         System.out.println("Your name is : " + firstName);
