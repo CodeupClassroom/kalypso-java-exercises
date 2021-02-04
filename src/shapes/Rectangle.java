ed359664ceb933c305f9373dbc6f905afc586479
Daniel Fryar <danielfryar@daniels-air.lan>
2021-02-04 16:50:27 -0600

    Finished Abstract Classes walkthrough

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,28 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+public class Rectangle extends Quadrilateral implements Measurable{
 
-    public Rectangle (double length, double width){
-        this.width = width;
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    void setLength(double length) {
         this.length = length;
     }
 
-    public double getArea(){
-        return this.length * this.width;
-    }
-
-    public double getPerimeter(){
-        return (2 * this.width) + (2 * this.length);
+    @Override
+    void setWidth(double width) {
+        this.width = width;
     }
 }
